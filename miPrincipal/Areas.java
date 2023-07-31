cfaee6c12ce026f1f13a1e72bd2abc0303a73283
Daniel Esparza Soto <137222659+EsparzaDaniel@users.noreply.github.com>
2023-07-31 14:23:36 -0700

    Initial commit

---
+++ miPrincipal/Areas.java
@@ -0,0 +1,20 @@
+package miPrincipal;
+
+public class Areas {
+
+    public int Cuadrado(int b) {
+        return b * b;
+    }
+    
+    public int Rectangulo(int b, int h) {
+        return b * h ;
+    }
+
+    public int Triangulo(int b, int h) {
+        return b * h / 2 ;
+    }
+
+    public int Romboide(int b, int h) {
+        return b * h ;
+    }
+}
---
+++ miPrincipal/Principal.java
@@ -0,0 +1,31 @@
+package miPrincipal;
+
+import java.util.Scanner;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        Areas a = new Areas();
+        int b,h;
+
+        Scanner consola = new Scanner(System.in);
+
+        System.out.println("Calculo de Areas");
+        System.out.println("======================");
+
+        System.out.print("Introduce la base:");
+        b = consola.nextInt();
+        
+        System.out.print("Introduce la altura:");
+        h = consola.nextInt();
+
+        System.out.println("El área de un cuadrado con un lado de " + b + "es " + a.Cuadrado( b ));
+        System.out.println("El área de un cuadrado con un lado de " + h + "es " + a.Cuadrado( h ));
+        
+        System.out.println("El área de un rectángulo con base de " + b + " y altura de " + h +  " es " + a.Rectangulo( b,h ));
+        System.out.println("El área de un triángulo con base de " + b + " y altura de " + h +  " es " + a.Triangulo( b,h ));
+        System.out.println("El área de un romboide con base de " + b + " y altura de " + h +  " es " + a.Romboide( b,h ));
+        
+        consola.close();
+    }
+}
